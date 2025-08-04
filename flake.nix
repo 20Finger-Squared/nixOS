901e3fbb50c36a69cb7056f9b4248476690aed40
20FingerSquared <orhylie@proton.com>
2025-08-04 11:06:49 +0100

    Pin home-manager to release-25.05 to fix version mismatch
    - Update flake.nix to use home-manager/release-25.05 branch
    - Update flake.lock with corresponding commit (fc3add4)
    - Resolves Home Manager 25.11 vs Nixpkgs 25.05 version warning

--- flake.nix
+++ flake.nix
@@ -1,33 +1,33 @@
 {
-  description = "Rhylie's NixOS configuration";
+	description = "Rhylie's NixOS configuration";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-25.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
 
-  outputs = { self, nixpkgs, home-manager }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      nixosConfigurations = {
-        rhyliePC = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.rhylie = import /home/rhylie/nixOS/home.nix;
-            }
-          ];
-        };
-      };
-    };
+	outputs = { self, nixpkgs, home-manager }:
+		let
+		system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+	in
+	{
+		nixosConfigurations = {
+			rhyliePC = nixpkgs.lib.nixosSystem {
+				inherit system;
+				modules = [
+					./configuration.nix
+						home-manager.nixosModules.home-manager
+						{
+							home-manager.useGlobalPkgs = true;
+							home-manager.useUserPackages = true;
+							home-manager.users.rhylie = import ./home.nix;
+						}
+				];
+			};
+		};
+	};
 }
