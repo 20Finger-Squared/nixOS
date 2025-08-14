08f2e2cbddaf8f7b0e801d7b7a2c01eddf709e65
20FingerSquared <orhylie@protonmail.com>
2025-08-15 00:15:15 +0100

    Added seperate keymaps file

---
+++ config/neovim/keymaps.nix
@@ -0,0 +1,20 @@
+{ ... }:
+''
+-- Disable arrow keys in normal mode
+vim.keymap.set('n', '<Up>', '<Nop>')
+vim.keymap.set('n', '<Down>', '<Nop>')
+vim.keymap.set('n', '<Left>', '<Nop>')
+vim.keymap.set('n', '<Right>', '<Nop>')
+
+-- Disable arrow keys in insert mode (optional)
+vim.keymap.set('i', '<Up>', '<Nop>')
+vim.keymap.set('i', '<Down>', '<Nop>')
+vim.keymap.set('i', '<Left>', '<Nop>')
+vim.keymap.set('i', '<Right>', '<Nop>')
+
+-- Disable arrow keys in visual mode (optional)
+vim.keymap.set('v', '<Up>', '<Nop>')
+vim.keymap.set('v', '<Down>', '<Nop>')
+vim.keymap.set('v', '<Left>', '<Nop>')
+vim.keymap.set('v', '<Right>', '<Nop>')
+''
