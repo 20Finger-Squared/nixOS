68f9ba0d5c74bab616e8630a1650d4c10fe1b8ac
20Finger-Squared <orhylie@gmail.com>
2025-08-20 16:24:52 +0100

    Formating

--- config/programs/neovim/opts.nix
+++ config/programs/neovim/opts.nix
@@ -2,136 +2,112 @@
 # lua
 ''
 
-    local opt = vim.opt
+  local opt = vim.opt
 
-    vim.g.mapleader = " "
-    vim.g.maplocalleader = " "
+  vim.g.mapleader = " "
+  vim.g.maplocalleader = " "
 
-    -- General
-    opt.wrap = false
-    opt.lazyredraw = true
-    opt.number = true
-    opt.relativenumber = true
-    opt.cursorline = true
-    opt.spell = true
-    opt.inccommand = 'split'
-    opt.confirm = true
+  -- General
+  opt.wrap = false
+  opt.lazyredraw = true
+  opt.number = true
+  opt.relativenumber = true
+  opt.cursorline = true
+  opt.spell = true
+  opt.inccommand = 'split'
+  opt.confirm = true
+  opt.winborder = "rounded"
 
-    -- Tabs
-    opt.tabstop = 4
-    opt.shiftwidth = 4
-    opt.smarttab = false
-    opt.expandtab = true
-    opt.smartindent = true
+  -- Tabs
+  opt.tabstop = 4
+  opt.shiftwidth = 4
+  opt.smarttab = false
+  opt.expandtab = true
+  opt.smartindent = true
 
-    -- Swap file
-    opt.swapfile = false
+  -- Swap file
+  opt.swapfile = false
 
-    opt.termguicolors = true
-    opt.showmode = false
-    opt.laststatus = 3
+  opt.termguicolors = true
+  opt.showmode = false
+  opt.laststatus = 3
 
-    -- Search
-    opt.smartcase = true
-    opt.incsearch = true
-    opt.hlsearch = true
+  -- Search
+  opt.smartcase = true
+  opt.incsearch = true
+  opt.hlsearch = true
 
-    -- Backup & undo
-    opt.backup = false
-    opt.writebackup = false
-    opt.undofile = true
+  -- Backup & undo
+  opt.backup = false
+  opt.writebackup = false
+  opt.undofile = true
 
-    -- Scrolling & wrapping
-    opt.scrolloff = 4
-    opt.sidescrolloff = 4
-    opt.linebreak = true
+  -- Scrolling & wrapping
+  opt.scrolloff = 4
+  opt.sidescrolloff = 4
+  opt.linebreak = true
 
-    -- Performance
-    opt.updatetime = 3000
-    opt.timeoutlen = 5000
+  -- Performance
+  opt.updatetime = 3000
+  opt.timeoutlen = 5000
 
+  -- lsp configs
+  vim.lsp.inlay_hint.enable(true)
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-            callback = function()
-            vim.lsp.buf.format({
-                    async = false,
-                    timeout_ms = 2000,
-                    })
-            end,
-            })
-
-    -- remove trailspece on write
-    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-            pattern = {"*"},
-            callback = function()
-            local save_cursor = vim.fn.getpos(".")
-            pcall(function() vim.cmd [[%s/\s\+$//e]] end)
-            vim.fn.setpos(".", save_cursor)
-            end,
-            })
-
-    -- Highlight all words under the cursor
-    vim.api.nvim_create_autocmd({"CursorMoved", "CursorMovedI"}, {
-            callback = function()
-            pcall(vim.fn.matchdelete, vim.w.cursorword_match or -1)
-            local word = vim.fn.expand('<cword>')
-            if word ~= "" then
-            vim.w.cursorword_match = vim.fn.matchadd('CursorWord', [[\<]] .. word .. [[\>]])
             end
-            end
-            })
+
+  vim.api.nvim_create_autocmd("BufWritePre", {
+          callback = function()
+          vim.lsp.buf.format({
+                  async = false,
+                  timeout_ms = 2000,
+                  })
+          end,
+          })
+
+  -- remove trailspece on write
+  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+          pattern = {"*"},
+          callback = function()
+          local save_cursor = vim.fn.getpos(".")
+          pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+          vim.fn.setpos(".", save_cursor)
+          end,
+          })
+
+  -- Highlight all words under the cursor
+  vim.api.nvim_create_autocmd({"CursorMoved", "CursorMovedI"}, {
+          callback = function()
+          pcall(vim.fn.matchdelete, vim.w.cursorword_match or -1)
+          local word = vim.fn.expand('<cword>')
+          if word ~= "" then
+          vim.w.cursorword_match = vim.fn.matchadd('CursorWord', [[\<]] .. word .. [[\>]])
+          end
+          end
+          })
 
   vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-      local file_dir = vim.fn.expand("%:p:h")  -- directory of current buffer
-      -- Try to get git root
-      local git_root = vim.fn.systemlist("git -C " .. file_dir .. " rev-parse --show-toplevel 2>/dev/null")[1]
-      if git_root and git_root ~= "" then
-        -- Set tab-local directory to git root
-        vim.cmd("tcd " .. git_root)
-      end
-    end,
+  callback = function()
+    local file_dir = vim.fn.expand("%:p:h")  -- directory of current buffer
+    -- Try to get git root
+    local git_root = vim.fn.systemlist("git -C " .. file_dir .. " rev-parse --show-toplevel 2>/dev/null")[1]
+    if git_root and git_root ~= "" then
+      -- Set tab-local directory to git root
+      vim.cmd("tcd " .. git_root)
+    end
+  end,
   })
 
-    -- highlight yanking
-    vim.api.nvim_create_autocmd('TextYankPost', {
-            desc = 'Highlight when yanking (copying) text',
-            group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-            callback = function()
-            vim.hl.on_yank()
-            end,
-            })
+  -- highlight yanking
+  vim.api.nvim_create_autocmd('TextYankPost', {
+          desc = 'Highlight when yanking (copying) text',
+          group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+          callback = function()
+          vim.hl.on_yank()
+          end,
+          })
 
-    vim.api.nvim_set_hl(0, 'CursorWord', { underline = true })
-
-    -- Diagnostic Config
-    -- See :help vim.diagnostic.Opts
-    vim.diagnostic.config {
-        severity_sort = true,
-                      float = { border = 'rounded', source = 'if_many' },
-                      underline = { severity = vim.diagnostic.severity.ERROR },
-                      signs = vim.g.have_nerd_font and {
-                          text = {
-                              [vim.diagnostic.severity.ERROR] = '󰅚 ',
-                              [vim.diagnostic.severity.WARN] = '󰀪 ',
-                              [vim.diagnostic.severity.INFO] = '󰋽 ',
-                              [vim.diagnostic.severity.HINT] = '󰌶 ',
-                          },
-                      } or {},
-                      virtual_text = {
-                          source = 'if_many',
-                          spacing = 2,
-                          format = function(diagnostic)
-                              local diagnostic_message = {
-                                  [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                                  [vim.diagnostic.severity.WARN] = diagnostic.message,
-                                  [vim.diagnostic.severity.INFO] = diagnostic.message,
-                                  [vim.diagnostic.severity.HINT] = diagnostic.message,
-                              }
-                          return diagnostic_message[diagnostic.severity]
-                              end,
-                      },
-    }
+  vim.api.nvim_set_hl(0, 'CursorWord', { underline = true })
 
 
 ''
