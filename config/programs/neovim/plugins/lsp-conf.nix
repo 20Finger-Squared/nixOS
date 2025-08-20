c57d5f9f1c3e27e40d2c1f0a84b779aff7206d4d
20Finger-Squared <orhylie@gmail.com>
2025-08-20 02:00:03 +0000

    Added basedpyright lsp config

--- config/programs/neovim/plugins/lsp-conf.nix
+++ config/programs/neovim/plugins/lsp-conf.nix
@@ -4,39 +4,51 @@
   type = "lua";
   config = # lua
     ''
-          local lspconfig = require("lspconfig")
-
-          lspconfig.clangd.setup({})
-          lspconfig.nil_ls.setup({
+      local lspconfig = require("lspconfig")
+      lspconfig.clangd.setup({})
+      lspconfig.nil_ls.setup({
               settings = {
                   ['nil'] = {
-                      formatting = { command = { "nixfmt" },
-                      },
+                      formatting = { command = { "nixfmt" } },
                   },
               },
-          })
-lspconfig.lua_ls.setup({
-    settings = {
-        Lua = {
-            runtime = { version = 'LuaJIT' },
-            diagnostics = { globals = {'vim'} },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            },
-            telemetry = { enable = false },
-            format = {
-                enable = true,
-                defaultConfig = {
-                    indent_style = "space",
-                    indent_size = "2",
-                    max_width = "80",
-                }
-            }
-        },
-    },
-})
+      })
 
+      -- Changed from pyright to basedpyright
+      lspconfig.basedpyright.setup({
+          settings = {
+              basedpyright = {
+                  analysis = {
+                      inlayHints = {
+                          variableTypes = true,
+                          callArgumentNames = true
+                      },
+                      diagnosticMode = "openFilesOnly"  -- Fixed typo: "diagnositcMode"
+                  }
+              }
+          }
+      })
 
+      lspconfig.lua_ls.setup({
+              settings = {
+                  Lua = {
+                      runtime = { version = 'LuaJIT' },
+                      diagnostics = { globals = {'vim'} },
+                      workspace = {
+                          library = vim.api.nvim_get_runtime_file("", true),
+                          checkThirdParty = false,
+                      },
+                      telemetry = { enable = false },
+                      format = {
+                          enable = true,
+                          defaultConfig = {
+                              indent_style = "space",
+                              indent_size = "2",
+                              max_width = "80",
+                          }
+                      }
+                  },
+              },
+      })
     '';
 }
