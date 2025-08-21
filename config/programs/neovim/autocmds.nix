545b3c19d9a28f708911839d8e93f7837ab1ca0b
20Finger-Squared <orhylie@gmail.com>
2025-08-21 01:36:00 +0100

    Fixups

--- config/programs/neovim/autocmds.nix
+++ config/programs/neovim/autocmds.nix
@@ -1,70 +1,89 @@
 { ... }:
 # lua
 ''
-  qol = vim.api.nvim_create_augroup('qol', { clear = true }),
-  formatting = vim.api.nvim_create_augroup('formating', { clear = true }),
+  -- augroups
+  local qol = vim.api.nvim_create_augroup("qol", { clear = true })
+  local formatting = vim.api.nvim_create_augroup("formatting", { clear = true })
 
-  -- Your LSP folding override
-  vim.api.nvim_create_autocmd('LspAttach', {
+  -- LSP folding override
+  vim.api.nvim_create_autocmd("LspAttach", {
       group = qol,
       callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client:supports_method('textDocument/foldingRange') then
+          if client:supports_method("textDocument/foldingRange") then
               local win = vim.api.nvim_get_current_win()
-              vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+              vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+          end
+
+          -- LSP document highlight support
+          if client.server_capabilities.documentHighlightProvider then
+              vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
+                  group = qol,
+                  buffer = args.buf,
+                  callback = vim.lsp.buf.document_highlight,
+              })
+              vim.api.nvim_create_autocmd({"CursorMoved", "CursorMovedI"}, {
+                  group = qol,
+                  buffer = args.buf,
+                  callback = vim.lsp.buf.clear_references,
+              })
           end
       end,
   })
 
+  -- Format on save
   vim.api.nvim_create_autocmd("BufWritePre", {
-      group = formatting
+      group = formatting,
       callback = function()
-          vim.lsp.buf.format({ async = false, timeout_ms = 2000, })
-      end
+          vim.lsp.buf.format({ async = false, timeout_ms = 2000 })
+      end,
   })
 
-  -- remove trail-space on write
-  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  -- Remove trailing whitespace on save
+  vim.api.nvim_create_autocmd("BufWritePre", {
       group = formatting,
       callback = function()
           local save_cursor = vim.fn.getpos(".")
-          pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+          pcall(function() vim.cmd([[%s/\s\+$//e]]) end)
           vim.fn.setpos(".", save_cursor)
       end,
   })
 
-  -- Highlight all words under the cursor
+  -- Fallback: highlight word under cursor (if no LSP highlight available)
   vim.api.nvim_create_autocmd({"CursorMoved", "CursorMovedI"}, {
       group = qol,
       callback = function()
-          pcall(vim.fn.matchdelete, vim.w.cursorword_match or -1)
-          local word = vim.fn.expand('<cword>')
-          if word ~= "" then
-              vim.w.cursorword_match = vim.fn.matchadd('CursorWord', [[\<]] .. word .. [[\>]])
+          -- Only run if LSP highlighting didn’t set up for this buffer
+          if not vim.b.lsp_highlight then
+              pcall(vim.fn.matchdelete, vim.w.cursorword_match or -1)
+              local word = vim.fn.expand("<cword>")
+              if word ~= "" then
+                  local safe = vim.fn.escape(word, [[\]])
+                  vim.w.cursorword_match = vim.fn.matchadd("CursorWord", [[\V\<]] .. safe .. [[\>]])
+              end
           end
-      end
+      end,
   })
 
-  -- Automatically set Neovim working directory to the git repo if one exists
+  -- Auto set working directory to git root
   vim.api.nvim_create_autocmd("BufEnter", {
       group = qol,
       callback = function()
-          local file_dir = vim.fn.expand("%:p:h")  -- directory of current buffer
-          -- Try to get git root
+          local file_dir = vim.fn.expand("%:p:h")
           local git_root = vim.fn.systemlist("git -C " .. file_dir .. " rev-parse --show-toplevel 2>/dev/null")[1]
           if git_root and git_root ~= "" then
-              -- Set tab-local directory to git root
               vim.cmd("tcd " .. git_root)
           end
       end,
   })
 
-  -- highlight yanking
-  vim.api.nvim_create_autocmd('TextYankPost', {
-      desc = 'Highlight when yanking (copying) text',
-      group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-      callback = vim.hl.on_yank
+  -- Highlight on yank
+  vim.api.nvim_create_autocmd("TextYankPost", {
+      desc = "Highlight when yanking (copying) text",
+      group = qol,
+      callback = vim.hl.on_yank,
   })
 
-  vim.api.nvim_set_hl(0, 'CursorWord', { underline = true })
+  -- Highlight style for cursorword
+  vim.api.nvim_set_hl(0, "CursorWord", { underline = true })
 ''
