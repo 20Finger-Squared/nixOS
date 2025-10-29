26d285e8f9ff2688aa39e6e9c8fa36b47ded6383
20Finger-Squared <orhylie@gmail.com>
2025-10-29 04:42:42 +0000

    Split sway config to it's own file

---
+++ sway.nix
@@ -0,0 +1,92 @@
+{ config, pkgs, ... }:
+
+{
+  wayland.windowManager.sway = {
+    enable = true;
+    checkConfig = true;
+    wrapperFeatures.gtk = true;
+    config = {
+      colors = {
+        focused = {
+          background = "#3C3836";
+          border      = "#FBF1C7";
+          childBorder = "#EBDBB2";
+          indicator   = "#458588";
+          text        = "#FBF1C7";
+        };
+        unfocused = {
+          background = "#3C3836";
+          border      = "#BDAE93";
+          childBorder = "#A89984";
+          indicator   = "#458588";
+          text        = "#EBDBB2";
+        };
+        urgent = {
+          background = "#3C3836";
+          indicator   = "#458588";
+          text        = "#FBF1C7";
+          border      = "#CC241D";
+          childBorder = "#CC241D";
+        };
+
+        background = "#3C3836";
+      };
+
+      gaps = {
+        horizontal = 3;
+        outer = 2;
+        smartBorders = "on";
+        smartGaps = true;
+      };
+
+      fonts = {
+        names = ["JetbrainsMono NF"];
+        size = 11.0;
+      };
+
+      floating = {
+        border = 3;
+        titlebar = true;
+      };
+
+      focus = {
+        followMouse = true;
+        wrapping = "workspace";
+        mouseWarping = true;
+        newWindow = "none";
+      };
+
+      keybindings =
+        let
+        mod = config.wayland.windowManager.sway.config.modifier;
+      altMod = config.wayland.windowManager.sway.config.modifier+"+Shift";
+      in {
+        "${mod}+q" = "kill";
+        "${altMod}+c" = "reload";
+
+        "${mod}+t" = "exec kitty";
+        "${altMod}+t" = "exec firefox";
+      };
+
+      window = {
+        border = 3;
+        hideEdgeBorders = "smart";
+      };
+
+      defaultWorkspace = "1";
+
+# default apps
+      menu = "fuzzel";
+      terminal = "${pkgs.kitty}/bin/kitty";
+
+# key binds
+      modifier = "Mod4";
+      right = "h";
+      left  = "l";
+      up    = "k";
+      down  = "j";
+
+    };
+
+  };
+}
