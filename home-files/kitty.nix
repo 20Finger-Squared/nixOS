f05c83ab499b5b66cfdca9057491bda321e5018f
20Finger-Squared <orhylie@gmail.com>
2025-10-30 17:02:40 +0000

    Update border with formating and colors

--- home-files/kitty.nix
+++ home-files/kitty.nix
@@ -6,16 +6,13 @@
         font.size = 11;
         shellIntegration.enableBashIntegration = true;
 
-        colors = {
+        settings = {
             background = "#282828";
             foreground = "#ebdbb2";
+            cursor = "#ebdbb2";
             selection_background = "#3c3836";
             selection_foreground = "#ebdbb2";
-            cursor = "#ebdbb2";
-            cursor_text_color = "#282828";
-            url_color = "#83a598";
 
-# Normal colors
             color0  = "#282828";
             color1  = "#cc241d";
             color2  = "#98971a";
@@ -25,7 +22,6 @@
             color6  = "#689d6a";
             color7  = "#a89984";
 
-# Bright colors
             color8  = "#928374";
             color9  = "#fb4934";
             color10 = "#b8bb26";
@@ -37,9 +33,10 @@
         };
 
         extraConfig = ''
-# performance tuning
             input_delay 0
             sync_to_monitor yes
+            window_border_width 0
+            window_padding_width 0
             '';
     };
 
