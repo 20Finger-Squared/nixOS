61c7156e247287ef802565f2682db877d23bd861
20Finger-Squared <orhylie@gmail.com>
2025-10-30 17:01:44 +0000

    Add policies and formating

--- home-files/firefox.nix
+++ home-files/firefox.nix
@@ -1,20 +1,35 @@
-{ pkgs, ... }@inputs:
+{ inputs, pkgs, ... }:
 
 let
-  addons = inputs.firefox-addons.packages.${pkgs.system};
+addons = inputs.firefox-addons.packages.${pkgs.system};
 in
 {
     programs.firefox = {
-            enable = true;
-            programs.firefox.profiles.tf.extensions.packages = with addons; [
+        enable = true;
+
+        policies = {
+            ExtensionSettings = {
+                "*" = {
+                    installation_mode = "allowed";   # allows user to enable extensions
+                        default_area = "navbar";         # optional: makes icon visible
+                        private_browsing = true;         # optional: enable in private windows
+                };
+            };
+        };
+
+        profiles.tf = {
+            search.default = "ddg";
+            isDefault = true;
+            extensions.packages = with addons; [
                 vimium-c
-                gruvbox-dark-theme
-                ublock-origin
-                tree-style-tab
-                clearurls
-                private-relay
-                privacy-badger
-                consent-o-matic
+                    gruvbox-dark-theme
+                    ublock-origin
+                    tree-style-tab
+                    clearurls
+                    private-relay
+                    privacy-badger
+                    consent-o-matic
             ];
         };
+    };
 }
