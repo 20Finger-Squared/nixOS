f49859c78e768155393730ec899ec773ad2f487e
20Finger-Squared <orhylie@gmail.com>
2025-11-01 07:11:16 +0000

    Fix indentation

--- configuration.nix
+++ configuration.nix
@@ -95,7 +95,7 @@
             enable = true;
             extraPortals = with pkgs; [
                 xdg-desktop-portal-wlr
-                    xdg-desktop-portal-gtk
+                xdg-desktop-portal-gtk
             ];
         };
     };
