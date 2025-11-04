5ea53ec4095ad173de79df80ceb04059ee947a1b
20Finger-Squared <orhylie@gmail.com>
2025-11-04 16:07:37 +0000

    fix name and add gaps

--- dwl-wm/package.nix
+++ dwl-wm/package.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }:
 {
-        users.users.tf.package = let
+        users.users.tf.packages = let
             customDwlPackage = (pkgs.dwl.override {
                 configH = ./dwl/config.h;
              }).overrideAttrs (oldAttrs: {
                 patches = (oldAttrs.patches or []) ++ [
                     ./dwl-patches/warpcursor.patch
+                    ./dwl-patches/gaps.patch
                 ];
                 buildInputs = oldAttrs.buildInputs or [] ++ [ pkgs.libdrm pkgs.fcft ];
             });
