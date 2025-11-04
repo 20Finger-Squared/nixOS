e4baf8ac2e1b122bfaf91f78cd1a9594eba1d567
20Finger-Squared <orhylie@gmail.com>
2025-11-04 04:28:56 +0000

    Configure ly

--- services-security.nix
+++ services-security.nix
@@ -16,10 +16,21 @@
   };
 
 
-# Ly a simple login screen
-  services.displayManager.ly.enable = true;
   services.displayManager = {
     enable = true;
+    ly={
+        enable = true; # Ly a simple login screen
+        package = pkgs.ly;
+        settings = {
+                allow_empty_password = false;
+                animation = "doom";
+                auth_fails = 1;
+                box_title = "Best place to login";
+                clear_password = true;
+                clock = "%x";
+            };
+    };
+
     sessionPackages =
     [
     (pkgs.writeTextFile {
