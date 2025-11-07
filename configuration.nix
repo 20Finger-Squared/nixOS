be955d533faf027dfef1b7d782840d3b29e25d53
20Finger-Squared <orhylie@gmail.com>
2025-11-07 12:34:45 +0000

    Add garbage collection settings for nix-store

--- configuration.nix
+++ configuration.nix
@@ -90,6 +90,10 @@
 
     nix.settings = {
         experimental-features = [ "nix-command" "flakes" ];
+        gc = {
+            automatic = true;
+            options = "--delete-older-than 30d";
+        };
     };
 
 # Allow unfree packages
