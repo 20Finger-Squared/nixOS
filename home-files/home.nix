a4ebcc69722528a0a927c6df4551359424dd9c09
20Finger-Squared <orhylie@gmail.com>
2025-11-07 12:25:58 +0000

    Add xdg config settings to improve screen sharing

--- home-files/home.nix
+++ home-files/home.nix
@@ -49,4 +49,18 @@
 
   programs.git.userEmail = "orhylie@gmail.com";
   programs.git.userName  = "20Finger-Squared";
+
+    # XDG portal config
+  xdg.configFile."xdg-desktop-portal/portals.conf".text = ''
+    [preferred]
+    default=wlr;gtk
+  '';
+
+  xdg.configFile."xdg-desktop-portal-wlr/config".text = ''
+    [screencast]
+    max_fps=30
+    output_name=
+    chooser_type=simple
+    chooser_cmd=slurp -f %o -or
+  '';
 }
