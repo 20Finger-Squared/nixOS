6e424aa893494c50781780101e6987ace6b0fddb
20Finger-Squared <orhylie@gmail.com>
2025-11-09 04:25:23 +0000

    More suckless

--- suckless/package.nix
+++ suckless/package.nix
@@ -2,11 +2,17 @@
 {
   users.users.tf.packages =
     let
+
+      customdmenu = pkgs.dmenu.overrideAttrs (oldAttrs: rec {
+        src = ./dmenu;
+        #patches = (oldAttrs.patches or [ ]) ++ [ ];
+      });
+
       customWmenu = pkgs.wmenu.overrideAttrs (oldAttrs: rec {
-        src = ./wmenu/build/wmenu-run; # path to your copy of wmenu source
+        src = ./wmenu; # path to your copy of wmenu source
         # Or if you just patch the upstream, you might skip src override
 
-        patches = (oldAttrs.patches or [ ]) ++ [ ];
+        #patches = (oldAttrs.patches or [ ]) ++ [ ];
 
         buildInputs = (oldAttrs.buildInputs or [ ]) ++ [
           pkgs.wlroots # example: if wmenu needs a wayland layer
@@ -14,23 +20,26 @@
         ];
       });
 
-      customDwlPackage =
-        (pkgs.dwl.override {
-          configH = ./dwl/config.h;
-        }).overrideAttrs
-          (oldAttrs: {
-            patches = (oldAttrs.patches or [ ]) ++ [
-              ./dwl-patches/warpcursor.patch
-              ./dwl-patches/gaps.patch
-            ];
-            buildInputs = oldAttrs.buildInputs or [ ] ++ [
-              pkgs.libdrm
-              pkgs.fcft
-            ];
-          });
+      customDwlPackage = (pkgs.dwl.override { configH = ./dwl/config.h; }).overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or [ ]) ++ [
+          ./dwl-patches/warpcursor.patch
+          ./dwl-patches/gaps.patch
+        ];
+
+        buildInputs = oldAttrs.buildInputs or [ ] ++ [
+          pkgs.libdrm
+          pkgs.fcft
+        ];
+      });
     in
     [
       customDwlPackage
       customWmenu
+      customdmenu
     ];
+
+  services.xserver.windowManager.dwm.package = pkgs.dwm.overrideAttrs {
+    src = ./dwm;
+    patches = [ ];
+  };
 }
