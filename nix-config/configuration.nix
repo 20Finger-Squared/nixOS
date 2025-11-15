d88a3043f34c4920049d5c1d8917b6a5c4d4d608
20Finger-Squared <orhylie@gmail.com>
2025-11-15 01:04:39 +0000

    Move packages to environment and change config input to pkgs

--- nix-config/configuration.nix
+++ nix-config/configuration.nix
@@ -120,27 +120,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = [
-    # tiny unopinonated packages or those of which are not relevant to one particular user
-    pkgs.libreoffice
-
-    # wayland needs
-    pkgs.wl-clipboard
-    pkgs.xdg-desktop-portal
-
-    # dev setup for nixOS
-    pkgs.nixfmt-rfc-style # formatter for dot-nix
-    pkgs.nil
-
-    # cli tools
-    pkgs.tmux
-    pkgs.jq
-    pkgs.ripgrep
-    pkgs.fzf
-
-    pkgs.ly
-  ];
-
   programs.dconf.enable = true;
   programs.steam = {
     enable = true;
--- nix-config/environment.nix
+++ nix-config/environment.nix
@@ -1,7 +1,28 @@
-{ config, ... }:
+{ pkgs, ... }:
 
 {
   environment.sessionVariables = rec {
     XDG_BIN_HOME = "$HOME/.bin";
   };
+
+  environment.systemPackages = [
+    # tiny unopinonated packages or those of which are not relevant to one particular user
+    pkgs.libreoffice
+
+    # wayland needs
+    pkgs.wl-clipboard
+    pkgs.xdg-desktop-portal
+
+    # dev setup for nixOS
+    pkgs.nixfmt-rfc-style # formatter for dot-nix
+    pkgs.nil
+
+    # cli tools
+    pkgs.tmux
+    pkgs.jq
+    pkgs.ripgrep
+    pkgs.fzf
+
+    pkgs.ly
+  ];
 }
