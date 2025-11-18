dc8e869a1509778284d9ce3f430e51ef8248297d
20Finger-Squared <orhylie@gmail.com>
2025-11-18 01:13:43 +0000

    Remove breaking

--- nix-config/hardware.nix
+++ nix-config/hardware.nix
@@ -7,8 +7,4 @@
     enable = true;
     extraPackages = with pkgs; [ mesa ];
   };
-  rtkit.enable = true; # Enables RealtimeKit for audio apps to get realtime priorities
-  sudo.extraConfig = ''
-    Defaults insults
-  '';
 }
