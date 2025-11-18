b4a0af67be6a1d69d4cb660ff300a6b5fa204106
20Finger-Squared <orhylie@gmail.com>
2025-11-18 01:15:42 +0000

    Change variable scope

--- suckless/package.nix
+++ suckless/package.nix
@@ -1,39 +1,52 @@
 { pkgs, ... }:
-{
-  users.users.tf.packages =
-    let
+let
 
-      customWmenu = pkgs.wmenu.overrideAttrs (oldAttrs: rec {
-        src = ./wmenu; # path to your copy of wmenu source
-        # Or if you just patch the upstream, you might skip src override
+  customWmenu = pkgs.wmenu.overrideAttrs (oldAttrs: rec {
+    src = ./wmenu; # path to your copy of wmenu source
+    # Or if you just patch the upstream, you might skip src override
 
-        #patches = (oldAttrs.patches or [ ]) ++ [ ];
+    #patches = (oldAttrs.patches or [ ]) ++ [ ];
 
-        buildInputs = (oldAttrs.buildInputs or [ ]) ++ [
-          pkgs.wlroots # example: if wmenu needs a wayland layer
-          pkgs.libxkbcommon # example: keyboard input library
-        ];
-      });
+    buildInputs = (oldAttrs.buildInputs or [ ]) ++ [
+      pkgs.wlroots # example: if wmenu needs a wayland layer
+      pkgs.libxkbcommon # example: keyboard input library
+    ];
+  });
 
-      customDwlPackage = (pkgs.dwl.override { configH = ./dwl/config.h; }).overrideAttrs (oldAttrs: {
-        patches = (oldAttrs.patches or [ ]) ++ [
-          ./dwl-patches/warpcursor.patch
-          ./dwl-patches/gaps.patch
-        ];
+  customDwmPackage = pkgs.dwm.overrideAttrs (oldAttrs: rec {
+    src = ./dwm;
+    conf = ./dwm/config.h;
+    patches = [ ];
+  });
 
-        buildInputs = oldAttrs.buildInputs or [ ] ++ [
-          pkgs.libdrm
-          pkgs.fcft
-        ];
-      });
-    in
-    [
-      customDwlPackage
-      customWmenu
+  customDwlPackage = (pkgs.dwl.override { configH = ./dwl/config.h; }).overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or [ ]) ++ [
+      ./dwl-patches/warpcursor.patch
+      ./dwl-patches/gaps.patch
     ];
 
-  services.xserver.windowManager.dwm.package = pkgs.dwm.overrideAttrs {
-    src = ./dwm;
-    patches = [ ];
+    buildInputs = oldAttrs.buildInputs or [ ] ++ [
+      pkgs.libdrm
+      pkgs.fcft
+    ];
+  });
+in
+{
+  users.users.tf.packages = [
+    customDwlPackage
+    customWmenu
+  ];
+
+  services = {
+    picom.enable = true;
+    libinput.enable = true;
+    xserver = {
+      enable = true;
+      autoRepeatDelay = 200;
+      autoRepeatInterval = 32;
+      windowManager.qtile.enable = true;
+      windowManager.dwm.enable = true;
+      windowManager.dwm.package = customDwmPackage;
+    };
   };
 }
