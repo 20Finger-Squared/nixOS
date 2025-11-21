4d1808163f8c59cb575bf2e887335815b1312708
20Finger-Squared <orhylie@gmail.com>
2025-11-21 12:47:27 +0000

    Enable ly for pc config

--- nix-config/tf-pc/services.nix
+++ nix-config/tf-pc/services.nix
@@ -25,18 +25,18 @@
 
   displayManager = {
     enable = true;
-    # ly = {
-    #   enable = true; # Ly a simple login screen
-    #   package = pkgs.ly;
-    #   settings = {
-    #     allow_empty_password = false;
-    #     animation = "doom";
-    #     auth_fails = 1;
-    #     box_title = "Best place to login";
-    #     clear_password = true;
-    #     clock = "%x";
-    #   };
-    # };
+    ly = {
+      enable = true; # Ly a simple login screen
+      package = pkgs.ly;
+      settings = {
+        allow_empty_password = false;
+        animation = "doom";
+        auth_fails = 1;
+        box_title = "Best place to login";
+        clear_password = true;
+        clock = "%x";
+      };
+    };
 
     sessionPackages = [
       (pkgs.writeTextFile {
