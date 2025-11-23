48c9f59fd32a1da6cd7c15e1e4a675c589a2344d
20Finger-Squared <orhylie@gmail.com>
2025-11-23 02:03:58 +0000

    Create tmux conf

--- home/tf/tmux.nix
+++ home/tf/tmux.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 {
   home.packages = [ pkgs.tmux ];
+  programs.tmux = {
+    baseIndex = 0;
+    clock24 = true;
+    keyMode = "vi";
+    mouse = false;
+    prefix = "CTRL-Space";
+  };
 }
