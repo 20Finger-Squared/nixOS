1a678e804c0a6f9dd6f0fc0f87d330f9d0d07f0d
20Finger-Squared <orhylie@gmail.com>
2025-11-25 15:31:13 +0000

    Add default Keys list

--- modules/dwm-config-generator.nix
+++ modules/dwm-config-generator.nix
@@ -7,6 +7,142 @@
 with lib;
 let
   cfg = config.programs.dwm;
+
+  defaultKeys = [
+    {
+      modifier = "MODKEY";
+      key = "XK_b";
+      function = "togglebar";
+      argument = "0";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_j";
+      function = "focusstack";
+      argument = ".i = +1";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_k";
+      function = "focusstack";
+      argument = ".i = -1";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_i";
+      function = "incnmaster";
+      argument = ".i = +1";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_d";
+      function = "incnmaster";
+      argument = ".i = -1";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_h";
+      function = "setmfact";
+      argument = ".f = -0.05";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_l";
+      function = "setmfact";
+      argument = ".f = +0.05";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_Return";
+      function = "zoom";
+      argument = "0";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_Tab";
+      function = "view";
+      argument = "0";
+    }
+    {
+      modifier = "MODKEY|ShiftMask";
+      key = "XK_c";
+      function = "killclient";
+      argument = "0";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_t";
+      function = "setlayout";
+      argument = ".v = &layouts[0]";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_f";
+      function = "setlayout";
+      argument = ".v = &layouts[1]";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_m";
+      function = "setlayout";
+      argument = ".v = &layouts[2]";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_space";
+      function = "setlayout";
+      argument = "0";
+    }
+    {
+      modifier = "MODKEY|ShiftMask";
+      key = "XK_space";
+      function = "togglefloating";
+      argument = "0";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_0";
+      function = "view";
+      argument = ".ui = ~0";
+    }
+    {
+      modifier = "MODKEY|ShiftMask";
+      key = "XK_0";
+      function = "tag";
+      argument = ".ui = ~0";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_comma";
+      function = "focusmon";
+      argument = ".i = -1";
+    }
+    {
+      modifier = "MODKEY";
+      key = "XK_period";
+      function = "focusmon";
+      argument = ".i = +1";
+    }
+    {
+      modifier = "MODKEY|ShiftMask";
+      key = "XK_comma";
+      function = "tagmon";
+      argument = ".i = -1";
+    }
+    {
+      modifier = "MODKEY|ShiftMask";
+      key = "XK_period";
+      function = "tagmon";
+      argument = ".i = +1";
+    }
+    {
+      modifier = "MODKEY|ShiftMask";
+      key = "XK_q";
+      function = "quit";
+      argument = "0";
+    }
+  ];
+
   file = pkgs.writeText "config.h" /* c */ ''
     ${cfg.file.prepend}
     static const unsigned int borderpx = ${toString cfg.borderpx};
