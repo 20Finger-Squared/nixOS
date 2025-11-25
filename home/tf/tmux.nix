78bef0caa00a4ca31bca59281f3024cd800c08ca
20Finger-Squared <orhylie@gmail.com>
2025-11-25 15:34:45 +0000

    Add more config for tmux

--- home/tf/tmux.nix
+++ home/tf/tmux.nix
@@ -2,10 +2,22 @@
 {
   home.packages = [ pkgs.tmux ];
   programs.tmux = {
+    enable = true;
     baseIndex = 0;
     clock24 = true;
     keyMode = "vi";
     mouse = false;
-    prefix = "CTRL-Space";
+    prefix = "C-Space";
+    extraConfig = ''
+      set -g focus-events on
+      set -g status-style bg=default
+      set -g status-left-length 99
+      set -g status-right-length 99
+      set -g status-justify centre
+
+      set-option -sg escape-time 10
+      set-option -g focus-events on
+      set-option -g default-terminal "screen-256color"
+    '';
   };
 }
