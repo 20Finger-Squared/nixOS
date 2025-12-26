06234d08361e391d29bcfbf0a731c9fe5f2b66bb
20Finger-Squared <orhylie@gmail.com>
2025-12-26 02:13:42 +0000

    Fix untracked lock file and input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration for tf";
 
   inputs = {
-    nixpkgs-24-11.url = "github:NixOS/nixpkgs/5eb803c7b22ee6f4ef62cfd9605f2b2c2555502c";
+    nixpkgs-24-11.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; # Correct channel for NixOS 25
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
