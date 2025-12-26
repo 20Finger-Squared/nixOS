d2ae90757254d0c966e35071715bb17cbcd4698b
20Finger-Squared <orhylie@gmail.com>
2025-12-26 02:14:55 +0000

    Create startx .xinitrc file

--- modules/shared/services/x11.nix
+++ modules/shared/services/x11.nix
@@ -7,6 +7,22 @@
       autoRepeatInterval = 32;
       enable = true;
       xkb.layout = "us";
+      displayManager.startx = {
+        enable = true;
+        generateScript = true;
+        extraCommands = /* sh */ ''
+          #!/bin/sh
+          xrandr --output HDMI-1 --rotate left
+
+          # makes the clipboard append to a file for a dmenu script
+          while true; do
+            clipnotify && echo "$(xclip -selection clipboard -o)" >> ~/.clipboard_history
+          done &
+
+          # Start dwm
+          exec dwm
+        '';
+      };
     };
   };
 }
