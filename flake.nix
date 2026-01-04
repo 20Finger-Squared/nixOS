6304c7111811055c35f53199b5a50c2ed98dfe03
20Finger-Squared <orhylie@gmail.com>
2026-01-04 20:50:47 +0000

    Merge flake.nix and outputs.nix

--- flake.nix
+++ flake.nix
@@ -8,5 +8,43 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: import ./outputs.nix inputs;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-24-11,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      system-type = "x86_64-linux";
+      mkSystem =
+        hostname: system-type:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+            nixpkgs-24-11 = nixpkgs-24-11.legacyPackages."${system-type}";
+          };
+          system = system-type;
+          modules = [
+            ./custom-modules/dwm/module.nix
+            ./custom-modules/dmenu/module.nix
+            ./custom-modules/general/colorscheme-module.nix
+            ./custom-modules/st/module.nix
+
+            ./modules/shared/default.nix
+            ./modules/${hostname}/hardware-configuration.nix
+            ./modules/${hostname}/default.nix
+
+            ./home/default.nix
+            home-manager.nixosModules.home-manager
+          ];
+        };
+    in
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+
+      nixosConfigurations.tf-laptop = mkSystem "tf-laptop" system-type;
+      nixosConfigurations.tf-pc = mkSystem "tf-pc" system-type;
+    };
 }
