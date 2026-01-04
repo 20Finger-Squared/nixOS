fe324afd3e309ac663f41cffc34d5bcb826ff013
20Finger-Squared <orhylie@gmail.com>
2026-01-04 00:28:41 +0000

    undo x11 config

--- modules/shared/services/x11.nix
+++ modules/shared/services/x11.nix
@@ -1,30 +1,4 @@
-{ lib, pkgs, ... }:
-let
-  dwm-statusbar = pkgs.writeShellScriptBin "dwm-status" ''
-    #!/usr/bin/env bash
-    while true;
-    do
-        xsetroot -name " $(date '+%X | %A | %d %B') "
-        sleep 1
-    done
-  '';
-  dot-xinitrc = /* sh */ ''
-    #!/usr/bin/env bash
-    # xrandr --output HDMI-1 --rotate left
-    #
-    # # script to update dwm-statusbar
-    # ${dwm-statusbar} &
-    #
-    #
-    # # makes the clipboard append to a file for a dmenu script
-    # while true; do
-    #   clipnotify && echo "$(xclip -selection clipboard -o)" >> ~/.clipboard_history
-    # done &
-    #
-    # Start dwm
-    exec ${lib.getExe pkgs.dwm}
-  '';
-in
+{ ... }:
 {
   services = {
     libinput.enable = true;
@@ -33,12 +7,6 @@ in
       autoRepeatInterval = 32;
       enable = true;
       xkb.layout = "us";
-      windowManager.dwm.enable = true;
-      displayManager.startx = {
-        enable = true;
-        generateScript = true;
-        extraCommands = dot-xinitrc;
-      };
     };
   };
 }
