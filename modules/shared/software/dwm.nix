8e49f02ca02b94d3fb5c829dcd5de3899528dc06
20Finger-Squared <orhylie@gmail.com>
2026-01-04 20:55:04 +0000

    Update dwm config

--- modules/shared/software/dwm.nix
+++ modules/shared/software/dwm.nix
@@ -1,4 +1,7 @@
-{ config, ... }:
+{
+  config,
+  ...
+}:
 let
   colorscheme = config.colorscheme;
   cfg = config.programs.dwm;
@@ -9,20 +12,23 @@ let
 in
 {
   programs.dwm = {
+    tagKeys.definitions = [ ];
     enable = true;
     modifier = "Mod4Mask";
-    terminal.launchKey = "XK_space";
-    terminal.modifier = "${modifer}";
-    terminal.appCmd = "st";
+    snap = 16;
+    borderpx = 3;
+
+    terminal = {
+      launchKey = "XK_space";
+      modifier = "${modifer}";
+      appCmd = "st";
+    };
     font = {
       size = 12;
       name = "JetbrainsMono NF";
     };
     appLauncher.launchKey = "XK_Tab";
     appLauncher.appArgs = [
-      {
-        flag = "-c";
-      }
       {
         flag = "-m";
         argument = ''dmenumon'';
@@ -36,13 +42,40 @@ in
         argument = ''"${cfg.font.name}:size=14"'';
       }
     ];
-    layout.mfact = 0.60;
-    snap = 16;
-    borderpx = 3;
-    patches.gaps = {
-      enable = true;
-      width = 8;
+
+    patches = {
+      cool-autostart = {
+        enable = true;
+        autostart = [
+          {
+            cmd = "st";
+            args = null;
+          }
+          {
+            cmd = "blueman-applet";
+            args = null;
+          }
+          {
+            cmd = "dwm-script";
+            args = null;
+          }
+        ];
+      };
+      keymodes = {
+        enable = true;
+        scheme = {
+          enable = true;
+          fg = "#${colorscheme.base07}";
+          bg = "#${colorscheme.base01}";
+          border = "#${colorscheme.base0D}";
+        };
+      };
+      gaps = {
+        enable = true;
+        width = 8;
+      };
     };
+
     colors = {
       SchemeNorm = {
         fg = "#${colorscheme.base05}";
@@ -55,6 +88,7 @@ in
         border = "#${colorscheme.base0A}";
       };
     };
+
     key = {
       useDefault = false;
       keys = [
@@ -69,144 +103,6 @@ in
           key = XF86AudioLowerVolume;
           function = "spawn";
           argument = ''SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ ${toString volumeIncrement}-")'';
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_t";
-          function = "spawn";
-          argument = ''SHCMD("qutebrowser")'';
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_b";
-          function = "togglebar";
-          argument = "{0}";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_j";
-          function = "focusstack";
-          argument = "{.i = +1 }";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_k";
-          function = "focusstack";
-          argument = "{.i = -1 }";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_i";
-          function = "incnmaster";
-          argument = "{.i = +1 }";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_d";
-          function = "incnmaster";
-          argument = "{.i = -1 }";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_h";
-          function = "setmfact";
-          argument = "{.f = -0.05}";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_l";
-          function = "setmfact";
-          argument = "{.f = +0.05}";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_Return";
-          function = "zoom";
-          argument = "{0}";
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_Tab";
-          function = "view";
-          argument = "{0}";
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_c";
-          function = "killclient";
-          argument = "{0}";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_t";
-          function = "setlayout";
-          argument = "{.v = &layouts[0]}";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_f";
-          function = "setlayout";
-          argument = "{.v = &layouts[1]}";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_m";
-          function = "setlayout";
-          argument = "{.v = &layouts[2]}";
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_Return";
-          function = "setlayout";
-          argument = "{0}";
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_space";
-          function = "togglefloating";
-          argument = "{0}";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_0";
-          function = "view";
-          argument = "{.ui = ~0 }";
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_0";
-          function = "tag";
-          argument = "{.ui = ~0 }";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_comma";
-          function = "focusmon";
-          argument = "{.i = -1 }";
-        }
-        {
-          modifier = "MODKEY";
-          key = "XK_period";
-          function = "focusmon";
-          argument = "{.i = +1 }";
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_comma";
-          function = "tagmon";
-          argument = "{.i = -1 }";
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_period";
-          function = "tagmon";
-          argument = "{.i = +1 }";
-        }
-        {
-          modifier = "MODKEY|ShiftMask";
-          key = "XK_q";
-          function = "quit";
-          argument = "{0}";
         }
       ];
     };
