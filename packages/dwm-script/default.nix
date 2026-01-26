1da7181b02fb08facbb464286415215aab4862cf
Rhylie M. Orton <orhylie+git@gmail.com>
2026-01-26 17:54:32 +0000

    feat: Update package to use gh repo

--- packages/dwm-script/default.nix
+++ packages/dwm-script/default.nix
@@ -1,12 +1,17 @@
-{ pkgs }:
+{
+  pkgs,
+}:
 pkgs.stdenv.mkDerivation {
   name = "dwm-script";
   pname = "dwm-script";
-  src = ./.;
+  src = pkgs.fetchgit {
+    url = "https://github.com/20Finger-Squared/dwm-script.git";
+    rev = "ca44367";
+    sha256 = "sha256-ESRz7vFti3bi/GfuTBbDAQmdKSix4GpBXaTygUS4ljc";
+  };
 
   nativeBuildInputs = [ pkgs.clang ];
-  buildInputs = [ ];
 
-  buildPhase = "clang -O3 dwm.c -o dwm-script";
+  buildPhase = "$CC -O3 dwm.c -o dwm-script";
   installPhase = "mkdir -p $out/bin; install -t $out/bin dwm-script";
 }
