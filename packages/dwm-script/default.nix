dfb2d5e2e4839fdad8ad3539119b7ac0f4bce6ff
Rhylie M. Orton <orhylie+git@gmail.com>
2026-01-31 00:19:49 +0000

    pkg: update dwm-script rev and sha256

--- packages/dwm-script/default.nix
+++ packages/dwm-script/default.nix
@@ -6,8 +6,8 @@ pkgs.stdenv.mkDerivation {
   pname = "dwm-script";
   src = pkgs.fetchgit {
     url = "https://github.com/20Finger-Squared/dwm-script.git";
-    rev = "ca44367";
-    sha256 = "sha256-ESRz7vFti3bi/GfuTBbDAQmdKSix4GpBXaTygUS4ljc";
+    rev = "2ee3d97";
+    sha256 = "sha256-k4G7q2jzbh8duKgaOexhQHdI4prFM0uaJmgDg2KIezk";
   };
 
   nativeBuildInputs = [ pkgs.clang ];
