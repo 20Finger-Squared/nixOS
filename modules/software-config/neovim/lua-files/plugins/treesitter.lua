5953f9e46de351bc40d1074a52b46cfdd815582b
Rhylie M. Orton <orhylie+git@gmail.com>
2026-02-28 00:02:24 +0000

    fix: neovim treesitter configured incorectly

--- modules/software-config/neovim/lua-files/plugins/treesitter.lua
+++ modules/software-config/neovim/lua-files/plugins/treesitter.lua
@@ -1,14 +1,9 @@
-require'nvim-treesitter'.setup {
-  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-  install_dir = vim.fn.stdpath('data') .. '/site'
-}
-require("nvim-treesitter.config").setup({
-  ensure_installed = "maintained",
-          highlight = { enable = true },
-          rainbow = {
-            enable = true,
-            extended_mode = true,
-            max_file_lines = 1000,
-          },
-
+require("nvim-treesitter.configs").setup({
+    ensure_installed = "maintained",
+    highlight = { enable = true },
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = 1000,
+    },
 })
