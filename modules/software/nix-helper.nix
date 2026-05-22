87d662b0cedffc19d2acaf8116bcf5724d65acd4
Rhylie M. Orton <orhylie+git@gmail.com>
2026-05-22 16:40:54 +0100

    add some better gc settings

--- modules/software/nix-helper.nix
+++ modules/software/nix-helper.nix
@@ -3,5 +3,9 @@ lib.mkSoftwareOption "nh" inputs {
   programs.nh = {
     enable = true;
     flake = "/home/tf/nixOS/";
+    clean = {
+      enable = true;
+      extraArgs = "--keep 10 --keep-since 7d";
+    };
   };
 }
--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,9 +1,24 @@
 { lib, ... }@inputs:
 lib.mkSystemOption "nix" inputs {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
   nixpkgs.config.allowUnfree = true;
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      keep-outputs = true;
+      keep-derivations = true;
+      max-jobs = "auto";
+      cores = 0;
+      warn-dirty = false;
+
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
+  };
 }
